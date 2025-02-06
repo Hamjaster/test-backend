4ca912e71e2ece123dd6254e3491d66fe92775e0
Hamza <hamzasepal@gmail.com>
2025-02-06 22:39:14 +0500

    changes

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import Razorpay from "razorpay";
+
+dotenv.config();
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cookieParser());
+app.use(cors());
+
+const instance = new Razorpay({
+  key_id: process.env.RZP_KEY_ID,
+  key_secret: process.env.RZP_KEY_SECRET,
+});
+
+app.get("/", function (req, res, next) {
+  res.json('API is running')
+  // res.render("index", { title: "Express" });
+});
+
+app.get("/api/v1/rzp_capture/:payment_id", (req, res) => {
+  const { payment_id } = req.params;
+
+  instance.payments
+    .capture(payment_id, FIXED_AMOUNT)
+    .then((data) => {
+      // Perform any action after successful payment
+      console.log("Payment successful:", data);
+
+      // Example: Save payment details to database or send confirmation email
+      // savePaymentToDB(data);
+      // sendEmailConfirmation(data.email);
+
+      res.json({
+        success: true,
+        message: "Payment captured successfully",
+        data,
+      });
+    })
+    .catch((error) => {
+      console.error("Payment capture failed:", error);
+      res
+        .status(500)
+        .json({ success: false, message: "Payment capture failed", error });
+    });
+});
+
+app.listen("3000", () => console.log("Listening on port 3000"));
